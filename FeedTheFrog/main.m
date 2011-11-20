a7354a266a81c74aae8d5077a7b78084caecbc1e
Naveed Siddiqui <naveed@10eighteen.com>
2011-11-20 20:09:55 +0800

    First commit

---
+++ FeedTheFrog/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FeedTheFrog
+//
+//  Created by Muhammad Naveed Siddiqui on 20/11/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
